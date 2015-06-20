7baddb20431d7efdcd40a697af9c489948952492
Laurie Koudstaal <laurie.koudstaal@fastmail.fm>
2015-06-20 14:08:39 +1000

    Reads in test and training data.

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,23 @@
-## Constants
+## CONSTANTS
 FILE_URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 ZIP_FILE <- "dataset.zip"
 DATASET_DIR <- "UCI HAR Dataset"
+
+FEATURE_FILE <- file.path(DATASET_DIR, "features.txt")
+
+ACTIVITY_LABEL_FILE <- file.path(DATASET_DIR, "activity_labels.txt")
+
 TEST_DIR <- file.path(DATASET_DIR, "test")
 SUBJECT_TEST_FILE <- file.path(TEST_DIR, "subject_test.txt")
 X_TEST_FILE <- file.path(TEST_DIR, "X_test.txt")
 Y_TEST_FILE <- file.path(TEST_DIR, "y_test.txt")
 
-## Helper functions
+TRAINING_DIR <- file.path(DATASET_DIR, "train")
+SUBJECT_TRAINING_FILE <- file.path(TRAINING_DIR, "subject_train.txt")
+X_TRAINING_FILE <- file.path(TRAINING_DIR, "X_train.txt");
+Y_TRAINING_FILE <- file.path(TRAINING_DIR, "y_train.txt");
+
+## HELPER FUNCTIONS
 download_file <- function (url, filename) {
 	if (file.exists(filename)) return();
 	
@@ -24,12 +34,23 @@ unzip_file <- function (file) {
 	unzip(file)
 }
 
-## Script
+## SCRIPT
 download_file(FILE_URL, ZIP_FILE)
 
 unzip_file(ZIP_FILE)
+
+# Read in feature data
+features <- read.table(FEATURE_FILE)
+
+# Read in activity lables
+activity_labels <- read.table(ACTIVITY_LABEL_FILE)
 
 # Read in test data
 test_subject_ids <- read.table(SUBJECT_TEST_FILE)
 test_x <- read.table(X_TEST_FILE)
 test_y <- read.table(Y_TEST_FILE)
+
+# Read in training data
+training_subject_ids <- read.table(SUBJECT_TRAINING_FILE)
+training_x <- read.table(X_TRAINING_FILE)
+training_y <- read.table(Y_TRAINING_FILE)
