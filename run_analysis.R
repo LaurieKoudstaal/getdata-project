9bd0a6fc5a5e0f76b8da21e303c13b6ffe86ab5d
Laurie Koudstaal <laurie.koudstaal@fastmail.fm>
2015-06-20 13:16:07 +1000

    Updated script to read in some of the test data.

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,13 @@
+## Constants
+FILE_URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 ZIP_FILE <- "dataset.zip"
 DATASET_DIR <- "UCI HAR Dataset"
+TEST_DIR <- file.path(DATASET_DIR, "test")
+SUBJECT_TEST_FILE <- file.path(TEST_DIR, "subject_test.txt")
+X_TEST_FILE <- file.path(TEST_DIR, "X_test.txt")
+Y_TEST_FILE <- file.path(TEST_DIR, "y_test.txt")
 
+## Helper functions
 download_file <- function (url, filename) {
 	if (file.exists(filename)) return();
 	
@@ -17,10 +24,12 @@ unzip_file <- function (file) {
 	unzip(file)
 }
 
-
-download_file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
-	ZIP_FILE)
+## Script
+download_file(FILE_URL, ZIP_FILE)
 
 unzip_file(ZIP_FILE)
 
-
+# Read in test data
+test_subject_ids <- read.table(SUBJECT_TEST_FILE)
+test_x <- read.table(X_TEST_FILE)
+test_y <- read.table(Y_TEST_FILE)
